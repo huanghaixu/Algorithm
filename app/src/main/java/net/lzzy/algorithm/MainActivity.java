31544521a9989b403ad2ee109085407c1f8f9c2a
hzq <1829007308@qq.com>
2019-06-08 16:39:27 +0800

    直接选择排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -54,6 +54,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void directSort() {
         //todo:直接选择排序的具体实现
+        //分为有序区和无序区，每一趟都在无序区依次对比，记录对比区域的最小元素的位置
+        //然后把无序区第一个元素和所记录的最小元素进行交换，无序区少一个、有序区多一个，循环往复直至无序区
+        //元素数量为0
+        for (int i=0;i<items.length-1;i++){
+            int minPos=i;
+            for (int j=i+1;j<items.length;j++){
+                if (items[minPos].compareTo(items[j])>0){
+                    minPos=j;
+                }
+            }
+            swap(minPos,i);
+        }
+    }
+
+    private void swap(int m, int n) {
+        int tmp=items[m];
+        items[m]=items[n];
+        items[n]=tmp;
     }
 
     private void generateItems() {
